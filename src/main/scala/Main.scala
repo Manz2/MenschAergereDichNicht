55f3344e7e18f594c19dc1617dd632f2f989bc4b
Manz2 <92571626+Manz2@users.noreply.github.com>
2021-10-22 11:23:08 +0200

    interface

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,9 @@ object Madn {
     println("Hello world!")
   println(msg)
 
-  var x = 0;
+//Anzahl Spielfelder
+
+  var x = 0
   var länge = 20
 
   while (x < länge) {
@@ -13,5 +15,22 @@ object Madn {
     +----+
     """)
     x = x + 1;
+  }
+
+//Anzahl Spieler
+  var spieler = 1
+  var figuren = 1
+  x = 0;
+  var y = 0;
+
+  while (x < spieler * 4) {
+    while (y < figuren) {
+      print("""
+    +----+
+         |
+    +----+
+    """)
+    }
+    println()
   }
 }
