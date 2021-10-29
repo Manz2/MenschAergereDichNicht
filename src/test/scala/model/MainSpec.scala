84cd3a9ee520a1cbce1a4be0ac6786ce5398e2ec
Manz2 <92571626+Manz2@users.noreply.github.com>
2021-10-29 09:05:45 +0200

    changed

---
+++ src/test/scala/model/MainSpec.scala
@@ -0,0 +1,23 @@
+package Htwg.se.MADN.MenschAergereDichNicht.model
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class MainSpec extends AnyWordSpec:
+  "Main" should {
+    "have a bar as String of form '+----+'"  +
+    "|    |" + "+----+" in {
+      field() should be ("+----+" + "\n")
+    }
+    "have a scalable bar" in {
+      bar(1) should be ("+----+" + "\n")
+      bar(2) should be ("+----+----+" + "\n")
+    }
+  }
+  "have cells as String of form '|    |'" in {
+    cells() should be("|    |" + "\n")
+  }
+  "have scalable cells" in {
+    cells(1) should be("|    |" + "\n")
+    cells(2) should be("|    |    |" + "\n")
+  }
