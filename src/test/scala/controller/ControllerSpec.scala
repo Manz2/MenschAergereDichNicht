61231f6b81f355a4dec24cb482d08e2bb1039df6
Manz2 <92571626+Manz2@users.noreply.github.com>
2021-11-12 11:39:21 +0100

    plugins

--- src/test/scala/controller/ControllerSpec.scala
+++ src/test/scala/controller/ControllerSpec.scala
@@ -1,23 +1,29 @@
 package Documents.Htwg.se.MADN.MenschAergereDichNicht
-package test.controller
+package controller
 import model.{Player, Field, Home}
+import util.{Observer,Observable}
+import Controller.{Controller}
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
 class ControllerSpec extends AnyWordSpec with Matchers:
   "A Controller" when {
-    "observed by an Observer" schould {
+    "observed by an Observer" should {
       var z: Array[String] = new Array[String](2)
-      val smallfield = new field(2,z)
-      val smallHomefield = new home(z)
-    }
-    "create a big Filed of 10 cells" in {
-      var x: Array[String] = new Array[String](10)
-      val field2 = new Field(10,x)
-      field2.toString should contain only ('+', '-', '|', ' ', '\n')
-      field2.toString should contain allOf ('+', '-', '|', ' ', '\n')
-      field2.toString.count(_ == '+') should equal(22)
-      field2.toString.count(_ == '-') should equal(80)
-      field2.toString.count(_ == '|') should equal (11)
-    }
+      //val smallfield = new Field(2,z)
+      val smallHomefield = new Home(z)
+      val smallPlayerfield = new Player(z)
+      val observer = new Observer {
+        var updated: Boolean = false
+        def isUpdated: Boolean = updated
+         override def update: Unit = updated = true
+      }
+      Controller.add(Observable)
+      "notify its Observer after creation" in {
+        var z: Array[String] = new Array[String](2)
+        Controller.newGame(z,z,z)
+        Observer.updated should be(true)
+        //controller.grid.size should be(4)
+      }
   }
+}
