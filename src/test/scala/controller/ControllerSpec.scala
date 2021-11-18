9928d8854c9584f107d8660ede6ca7fb47066945
Manz2 <92571626+Manz2@users.noreply.github.com>
2021-11-18 11:44:18 +0100

    test

---
+++ src/test/scala/controller/ControllerSpec.scala
@@ -0,0 +1,30 @@
+package Documents.Htwg.se.MADN.MenschAergereDichNicht
+package Controller
+
+import model.{Player, Field, Home}
+import util.{Observer,Observable}
+//import Controller.Controller
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+class ControllerSpec extends AnyWordSpec with Matchers:
+  "A Controller" when {
+    "observed by an Observer" should {
+      var z: Array[String] = new Array[String](2)
+      //val smallfield = new Field(2,z)
+      val smallHomefield = new Home(z)
+      val smallPlayerfield = new Player(z)
+      val observer = new Observer {
+        var updated: Boolean = false
+        def isUpdated: Boolean = updated
+         override def update: Unit = updated = true
+      }
+      //Controller.add(Observable)
+      "notify its Observer after creation" in {
+        var z: Array[String] = new Array[String](2)
+        //Controller.newGame(z,z,z)
+        //Observer.updated should be(true)
+        //controller.grid.size should be(4)
+      }
+  }
+}
