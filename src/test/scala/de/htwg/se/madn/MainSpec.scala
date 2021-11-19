99e63ce35eec470159756d6003de92033c5a8f67
Manz2 <92571626+Manz2@users.noreply.github.com>
2021-11-19 08:09:32 +0100

    mainspec

---
+++ src/test/scala/de/htwg/se/madn/MainSpec.scala
@@ -0,0 +1,24 @@
+package de.htwg.se.madn
+
+import model.{Player, Field, Home}
+import util.{Observer,Observable}
+import Controller.Controller
+import aview.{Tui}
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class MainSpec extends AnyWordSpec with Matchers{
+  "The madn main class" should {
+    "create a new tui" in{
+      var init: Array[String] = new Array[String](1)
+      val home = new Home(init)
+      val player = new Player(init)
+      val field = new Field(1, init)
+      val controller = new Controller(home, player, field)
+      val tui = Tui(controller)
+    }
+  }
+}
