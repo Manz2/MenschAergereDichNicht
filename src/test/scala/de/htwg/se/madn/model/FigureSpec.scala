65ef9968b11b8c7ce345f2a3cc98a4f94f0e6752
Manz2 <erikmanzer21@gmail.com>
2023-03-27 22:22:56 +0200

    Figure test

---
+++ src/test/scala/de/htwg/se/madn/model/FigureSpec.scala
@@ -0,0 +1,21 @@
+package de.htwg.se.madn
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+import model.FigureComponent.FigureBaseImpl.Figure
+import model.FigureComponent.FigureInterface
+import scala.language.postfixOps
+
+class FigureSpec extends AnyWordSpec with Matchers:
+  "Figure" should {
+    "compare two similar Figures as true" in {
+      val figure1 = Figure("A",1)
+      val figure2 = Figure("A",1)
+      figure1.equals(figure2) should be(true)
+    }
+    "compare two different Figures as false" in {
+      val figure1 = Figure("A",1)
+      val figure2 = Figure("B",1)
+      figure1.equals(figure2) should be(false)
+    }
+  }
