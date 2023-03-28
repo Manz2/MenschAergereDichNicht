61eeec487115e41fa9c260f68f9323edde95d6cb
Manz2 <erikmanzer21@gmail.com>
2023-03-28 12:35:11 +0200

    some tui tests

--- src/test/scala/de/htwg/se/madn/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/madn/aview/TuiSpec.scala
@@ -1,11 +1,11 @@
-/*                                                                                      
+ /*                                                                                    
     /\      /\          /\       | | \ \      /\      / /
    /  \    /  \        /  \      | |  \ \    /  \    / /
   / /\ \  / /\ \      / /\ \     | |   | |  / /\ \  / /
  / /  \ \/ /  \ \    /      \    | |  / /  / /  \ \/ /
 / /    \  /    \ \  / /    \ \   | | / /  / /    \  / */                                                                                      
                                                                                      
-/*package de.htwg.se.madn
+package de.htwg.se.madn
 
 import aview.{Tui}
 import org.scalatest.wordspec.AnyWordSpec
@@ -16,12 +16,13 @@ import scala.util.{Try,Success,Failure}
 import util.Observer
 import Controller.controllerComponent.ControllerInterface
 import Controller.controllerComponent.controllerBaseImpl._
+import model.FigureComponent.FigureBaseImpl.Figure
+import model.FigureComponent.FigureInterface
 
 class TuiSpec extends AnyWordSpec with Matchers {
 
     "A Tui" should {
         "create an new madn Game with 1 player on input 'n1'" in {
-            var inserts: Array[Option[String]] = new Array[Option[String]](10)
             val co = new Controller()
             val tui = new Tui(co)
             tui.processInputLine("n1")
@@ -60,44 +61,43 @@ class TuiSpec extends AnyWordSpec with Matchers {
             val co = new Controller()
             val tui = new Tui(co)
             tui.processInputLine("a")
-            tui.turn should equal('a')
+            tui.turn should equal("A")
             tui.diceVal.toInt should be <= 6
         }
         "start a new round with player a's turn were he has no players in the field on input 'a'" in {
             val co = new Controller()
             val tui = new Tui(co)
-            tui.startGame(1)
             tui.processInputLine("a")
-            tui.turn should equal('a')
+            tui.turn should equal("A")
         }
         "start a new round with player b's turn on input 'b'" in {
             val co = new Controller()
             val tui = new Tui(co)
             tui.processInputLine("b")
-            tui.turn should equal('b')
+            tui.turn should equal("B")
             tui.diceVal.toInt should be <= 6
         }
         "start a new round with player c's turn on input 'c'" in {
             val co = new Controller()
             val tui = new Tui(co)
             tui.processInputLine("c")
-            tui.turn should equal('c')
+            tui.turn should equal("C")
             tui.diceVal.toInt should be <= 6
         }
         "start a new round with player d's turn on input 'd'" in {
             val co = new Controller()
             val tui = new Tui(co)
             tui.processInputLine("d")
-            tui.turn should equal('d')
+            tui.turn should equal("D")
             tui.diceVal.toInt should be <= 6
-        }
+        }/*
         "move figure 1 or leave player field with the next figure on input '1'" in {
             val co = new Controller()
             val tui = new Tui(co)
-            tui.startGame(1)
-            tui.diceVal = "6"
+            co.newGame(1)
+            tui.diceVal = 6
             tui.processInputLine("1")
-            co.field.figuren(0) should equal(Some("A1")) 
+            co.field.data(0).toString should equal(Figure("A",1).toString) 
         }
         "move figure 2 or leave player field with the next figure on input '2'" in {
             val co = new Controller()
@@ -122,7 +122,7 @@ class TuiSpec extends AnyWordSpec with Matchers {
             tui.diceVal = "6"
             tui.processInputLine("4")
             co.field.figuren(0) should equal(Some("A1")) 
-        }
+        }*/
     }
-}*/
+}
 
