d905c357f6202699028ad544b4290f419ebabe0e
Manz2 <erikmanzer21@gmail.com>
2023-04-05 10:16:32 +0200

    removed tools from main branch

--- Tools/main/scala/de/htwg/se/madn/model/FieldComponent/fieldBaseImpl/Strategy.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.se.madn
-package model.FieldComponent.fieldBaseImpl
-import model.FigureComponent.FigureInterface
-import model.FieldComponent.FieldInterface
-
-trait Strategy {
-  def move(figur:FigureInterface,anzahlFelder:Int,status:Boolean) :FieldInterface
-}
--- Tools/main/scala/de/htwg/se/madn/model/FigureComponent/FigureInterface.scala
+++
@@ -1,5 +0,0 @@
-package de.htwg.se.madn
-package model.FigureComponent
-import model.FigureComponent.FigureBaseImpl.Figure
-
-trait FigureInterface(val playerName:String, val number:Int,val state: Boolean=false) {}
